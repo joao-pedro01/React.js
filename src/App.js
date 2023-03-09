2c589687dd98104579442f886862403963386edd
joao-pedro01 <joao_pedro01@terra.com.br>
2023-03-09 17:42:06 -0300

    Condicional (if)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() {
   return (
     <div className='App'>
-      <Form />
-      <Evento />
+      <h1>Rendenização condicional</h1>
+      <Condicional />
     </div>
   );
 }
